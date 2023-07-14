d3586acc01cd7b3d3344d61b1afb6062cf3e3546
ArthurFer <arthurfernando18@gmail.com>
2023-07-14 00:52:18 -0300

    push

---
+++ Saque_caixa.py
@@ -0,0 +1,14 @@
+def sacar (valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("Operação efetuada com sucesso")
+        print("Retire o dinheiro")
+    else:
+        print("Saldo insuficiente")
+print("Obrigado por ser nosso cliente")
+
+
+print("Caixa Eletronico")
+
+
