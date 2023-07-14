573258ab15d31def908d9ca8f629a7b6f5e6b413
ArthurFer <arthurfernando18@gmail.com>
2023-07-14 01:08:10 -0300

    inclusao função

--- Saque_caixa.py
+++ Saque_caixa.py
@@ -1,14 +1,16 @@
 def sacar (valor):
     saldo = 500
 
-    if saldo >= valor:
+    if (saldo >= valor):
         print("Operação efetuada com sucesso")
         print("Retire o dinheiro")
     else:
         print("Saldo insuficiente")
-print("Obrigado por ser nosso cliente")
 
 
-print("Caixa Eletronico")
+print("Caixa Eletronico\n\n")
+saque = input("Informe o valor que deseja sacar: ")
+saque = float(saque)
+sacar(saque)
 
 
