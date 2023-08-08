daceec83d8bdd291145e7679d8ad117490aa9cfa
ArthurFer <arthurfernando18@gmail.com>
2023-08-08 17:56:53 -0300

    Alterações

--- Desafios_de_Codigos_DIO.py
+++ Desafios_de_Codigos_DIO.py
@@ -53,21 +53,71 @@
 # if __name__ == "__main__":
 #   main()
 
-numPedidos = int(input())
-lista_pratos = []
+# numPedidos = int(input())
+# lista_pratos = []
 
-for i in range(1, numPedidos + 1):
-    prato = input()
-    calorias = int(input())
-    ehVegano = False
-    condicional_vegano = input()
+# for i in range(1, numPedidos + 1):
+#     prato = input()
+#     calorias = int(input())
+#     ehVegano = False
+#     condicional_vegano = input()
     
-    if condicional_vegano == "s":
-      vegano = "(Vegano)"
-    else:
-      vegano = "(Nao-vegano)"
-    lista_pratos.append(f"Pedido {i}: {prato} {vegano} - {calorias} calorias")
+#     if condicional_vegano == "s":
+#       vegano = "(Vegano)"
+#     else:
+#       vegano = "(Nao-vegano)"
+#     lista_pratos.append(f"Pedido {i}: {prato} {vegano} - {calorias} calorias")
 
-for pedidos in lista_pratos:
-  print(pedidos)
-  print()
+# for pedidos in lista_pratos:
+#   print(pedidos)
+#   print()
+
+# T = str(input())
+
+# if len(T) <= 140:
+#     print("TWEET")
+# else:
+#     print("MUTE") 
+
+# month = input()
+
+# months_dict = {
+#     "1": "January",
+#      "2": "February",
+#      "3": "March",
+#      "4": "April",
+#      "5": "May",
+#      "6": "June",
+#      "7": "July",
+#      "8": "August",
+#      "9": "September",
+#      "10": "October",
+#      "11": "November",
+#      "12": "December"
+# }
+
+# if month == "1":
+#     print(months_dict["1"])
+# elif month == "2":
+#     print(months_dict["2"])
+# elif month == "3":
+#     print(months_dict["3"])
+# elif month == "4":
+#     print(months_dict["4"])
+# elif month == "5":
+#     print(months_dict["5"])
+# elif month == "6":
+#     print(months_dict["6"])
+# elif month == "7":
+#     print(months_dict["7"])
+# elif month == "8":
+#     print(months_dict["8"])
+# elif month == "9":
+#     print(months_dict["9"])
+# elif month == "10":
+#     print(months_dict["10"])
+# elif month == "11":
+#     print(months_dict["11"])
+# elif month == "12":
+#     print(months_dict["12"])
+
