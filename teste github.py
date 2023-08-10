359cc504e0a60c9403f5e9b078547b03aa3b77eb
ArthurFer <arthurfernando18@gmail.com>
2023-08-10 01:24:48 -0300

    Comentario

--- teste github.py
+++ teste github.py
@@ -1,2 +1,11 @@
 print("Teste github")
 print ("Alteração feita no navegador github")
+
+#Teste dicionario 
+# animais = {
+#     "vertebrado":{"ave":{"carnivoro":"aguia", "onivoro":"pomba"},"mamifero":{"onivoro":"homem", "herbivoro": "vaca"}},
+#     "invertebrado":{"inseto":{"hematofago":"pulga", "herbivoro":"lagarta"},"anelideo":{"hematofago":"sanguessuga", "onivoro": "minhoca"}}
+
+#     }
+
+# print(animais["vertebrado"]["ave"])
